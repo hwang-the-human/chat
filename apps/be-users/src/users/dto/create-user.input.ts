8e27ee1a512afbd5e5be364c18016d69fd75321e
Nikolay Khvan <hwang.the.human@gmail.com>
2023-10-01 19:50:19 +0600

    feat: add validation

--- apps/be-users/src/users/dto/create-user.input.ts
+++ apps/be-users/src/users/dto/create-user.input.ts
@@ -1,10 +1,13 @@
 import { InputType, Field } from '@nestjs/graphql';
+import { IsAlpha } from 'class-validator';
 
 @InputType()
 export class CreateUserInput {
+  @IsAlpha()
   @Field()
   first_name: string;
 
+  @IsAlpha()
   @Field()
   last_name: string;
 
