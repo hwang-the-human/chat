78ef1ee7656fe4e89d6edb29316a6fb1e5e7a79c
Nikolay Khvan <hwang.the.human@gmail.com>
2023-12-09 08:34:13 +0600

    fix: importings in auth service

--- apps/be-auth/src/app/app.module.ts
+++ apps/be-auth/src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import * as SuperTokensConfig from './config';
+import { AuthModule } from '../auth/auth.module';
+import * as SuperTokensConfig from '../configs/superTokens.config';
 
 @Module({
   imports: [
--- apps/be-auth/src/auth/supertokens/supertokens.service.ts
+++ apps/be-auth/src/auth/supertokens/supertokens.service.ts
@@ -1,7 +1,7 @@
 import { Inject, Injectable } from '@nestjs/common';
 import supertokens from 'supertokens-node';
 import { ConfigInjectionToken, AuthModuleConfig } from '../config.interface';
-import * as SuperTokensConfig from '../../config';
+import * as SuperTokensConfig from '../../configs/superTokens.config';
 
 @Injectable()
 export class SupertokensService {
