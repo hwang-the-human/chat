38f39adab20400e753a15d6f0ccf7f767b445a4a
Nikolay Khvan <hwang.the.human@gmail.com>
2023-12-09 08:04:06 +0600

    feat: add supertokens to fe

--- apps/fe-web-chat/src/app/app.tsx
+++ apps/fe-web-chat/src/app/app.tsx
@@ -1,16 +1,41 @@
-import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import SuperTokens, { SuperTokensWrapper } from 'supertokens-auth-react';
+import { getSuperTokensRoutesForReactRouterDom } from 'supertokens-auth-react/ui';
+import { SessionAuth } from 'supertokens-auth-react/recipe/session';
+import { Routes, Route } from 'react-router-dom';
+import {
+  PreBuiltUIList,
+  SuperTokensConfig,
+  ComponentWrapper,
+} from '../configs/superTokens.config';
 import Main from '../components/pages/main/main';
-import Login from '../components/pages/auth/login';
-import Registration from '../components/pages/auth/registration';
+
+SuperTokens.init(SuperTokensConfig);
 
 export default function App() {
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Registration />} />
-      </Routes>
-    </div>
+    <SuperTokensWrapper>
+      <ComponentWrapper>
+        <div className="App app-container">
+          <div className="fill">
+            <Routes>
+              {getSuperTokensRoutesForReactRouterDom(
+                require('react-router-dom'),
+                PreBuiltUIList
+              )}
+
+              <Route
+                path="/"
+                element={
+                  <SessionAuth>
+                    <Main />
+                  </SessionAuth>
+                }
+              />
+            </Routes>
+          </div>
+        </div>
+      </ComponentWrapper>
+    </SuperTokensWrapper>
   );
 }
